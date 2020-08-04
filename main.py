290cd8734d10f8ad666926251936238fbfc51902
yumitomo <yaonuma@umich.edu>
2020-08-04 19:23:31 -0400

    some EDA

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 10000)
 
-#contraction dictionary
+
 CONTRACTION_MAP = {
     "ain`t": "is not",
     "aren`t": "are not",
