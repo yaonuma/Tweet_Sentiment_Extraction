e85cb8de50cf204eda4a6cc1e1cb4d4d1e2bb80f
yumitomo <yaonuma@umich.edu>
2020-08-04 01:57:04 -0400

    test new commit

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 10000)
 
+#new commit
 
 CONTRACTION_MAP = {
     "ain`t": "is not",
